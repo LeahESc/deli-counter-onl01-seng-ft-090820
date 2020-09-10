c7f7cd82f0c45f36daa74f520b3ecedd1a64de81
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 21:59:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 def line(katz_deli, person)
   if katz_deli.size == 0 
     return "The line is empty"
-  else katz_deli.each_with_index.map |name, index|
-    #{index+1} + #{name}
+  else katz_deli.each_with_index.map { |name, index|
+    return #{index+1} + #{name} }
   end
 end
     
