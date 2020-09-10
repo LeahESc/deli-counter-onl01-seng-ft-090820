ca7c17ccfec887fff5ef5347709ca71426d2349c
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 23:39:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,10 @@ end
 end
 
 def take_a_number(katz_deli, new_customers)
-    katz_deli.push(new_customers)
+    if katz_deli.size == 0 
+      katz_deli.push(new_customers)
+    if katz_deli.size > 0
+      katz_deli.insert(-1, new_customers)
     katz_deli.each_with_index do |name, index|
     puts"Welcome, #{name}. You are number #{index+1} in line."
     end
