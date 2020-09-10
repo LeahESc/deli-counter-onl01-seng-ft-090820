92848f35652575ed9505398886b184e097ee203a
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 22:05:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
-  else katz_deli.map_with_index(1) do |n , name|
+  elsif katz_deli.size >= 1 
+    katz_deli.map_with_index(1) do |n , name|
     puts #{name} #{n}
     end
   end
