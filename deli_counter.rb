f2f430ead491bbdfe7af877d9d2fcf8486ef6809
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 22:17:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
   elsif katz_deli.size >= 1 
     current_line = []
     katz_deli.each_with_index do |index , name|
-    current_line << puts ("The line is currently:" #{index+1}. #{name})
-    end
+    current_line << puts ("The line is currently:" #{index+1}. #{name}
     puts current_line.join (" ")
+  end
   end
 end
     
