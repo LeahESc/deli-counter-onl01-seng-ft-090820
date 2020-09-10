4244b4af9314535e9bc8d6f04fee8b1edd24bd48
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 22:35:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
-  elsif katz_deli.size >= 1
-  
+  elsif katz_deli.size >= 1 
+    current_line = []
     katz_deli.each_with_index do |index , name|
-    puts "The line is currently:#{index + 1} #{name}"
-    end
+    current_line << "#{index+1}  {name}"
+    puts "The line is currently:"current_line.join("  ")
   end
+end
 end
     
