564f9e044f093b7b3af60102914c65f4197dc100
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 23:55:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,7 @@ def take_a_number(katz_deli, new_customers)
   binding.pry
     if katz_deli.size == 0 
       katz_deli.push(new_customers)
+      puts "Welcome, #{name}. You are number #{index+1} in line."
     elsif katz_deli.size > 0
       katz_deli.insert(-1, new_customers)
     katz_deli.each_with_index do |name, index|
