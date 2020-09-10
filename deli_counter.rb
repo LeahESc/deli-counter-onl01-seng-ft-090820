0314eead29602adf03f75e736c8fe46bec38cbe7
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 23:52:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
@@ -10,7 +12,9 @@ def line(katz_deli)
 end
 end
 
+
 def take_a_number(katz_deli, new_customers)
+  binding.pry
     if katz_deli.size == 0 
       katz_deli.push(new_customers)
     elsif katz_deli.size > 0
