fed7d5ce20e2a2e2d765f48c40add3ebd1177669
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 23:28:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,6 @@ end
 
 def take_a_number(katz_deli, new_customers)
     katz_deli.insert(-1, "new_customers")
-    "new_customers" = "Ada"
     katz_deli.each_with_index do |name, index|
     puts"Welcome, #{name}. You are number #{index+1} in line."
     end
